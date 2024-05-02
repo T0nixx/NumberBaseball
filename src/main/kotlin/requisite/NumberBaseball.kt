1c9be25a8380b24072f003fd17a4e91c6259b924
TONIXX <jiseok95@gmail.com>
2024-05-02 12:51:23 +0900

    feat: notice about number string requirements

--- src/main/kotlin/requisite/NumberBaseball.kt
+++ src/main/kotlin/requisite/NumberBaseball.kt
@@ -32,6 +32,7 @@ class NumberBaseball {
                     println(validationResult.message)
                 }
                 println("정답이라고 생각하는 3자리 숫자를 입력해주세요")
+                println("0은 포함될 수 없고 각 자릿수는 중복되어서는 안됩니다.")
                 val userInput = readlnOrNull()
 
                 validationResult = Validator.validateUserInput(userInput)
