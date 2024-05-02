3612d7763b9e9f63f28776d94846179420bc18b9
TONIXX <jiseok95@gmail.com>
2024-05-02 20:40:20 +0900

    style: format with ktlint

--- src/main/kotlin/optional/validator/isIntegerValidator.kt
+++ src/main/kotlin/optional/validator/isIntegerValidator.kt
@@ -1,11 +1,13 @@
 package optional.validator
 
-val isIntegerValidator = StringValidator {
-    if (it.toIntOrNull() == null) {
-        ValidationFailure(
-            it,
-            "입력된 값 ${it}이 정수가 아닙니다.",
-        )
+val isIntegerValidator =
+    StringValidator {
+        if (it.toIntOrNull() == null) {
+            ValidationFailure(
+                it,
+                "입력된 값 ${it}이 정수가 아닙니다.",
+            )
+        } else {
+            ValidationSuccess(it)
+        }
     }
-    else ValidationSuccess(it)
-}
