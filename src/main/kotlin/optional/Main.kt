d663f2d3506e1559b1b4674f4e83bb0efbda5fcd
TONIXX <jiseok95@gmail.com>
2024-05-02 20:40:42 +0900

    fix: catch Exceptions too

--- src/main/kotlin/optional/Main.kt
+++ src/main/kotlin/optional/Main.kt
@@ -3,8 +3,11 @@ package optional
 fun main() {
     try {
         NumberBaseball(AnswerGenerator()).start()
-    } catch (e: Error) {
+    } catch (e: Exception) {
         println("처리되지 않은 예외입니다.")
+        println(e.message)
+        println(e.cause)
+    } catch (e: Error) {
         println(e.message)
         println(e.cause)
     }
